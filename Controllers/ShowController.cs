ea51766d950ccda19cb6678c5b5c52692e242469
LouiseCrowe <louise.crowe7@gmail.com>
2020-12-03 09:07:13 +0000

    commit 2 - renamed to ShowList.razor pulling data from API successfully, no sort or format yet

--- Controllers/ShowController.cs
+++ Controllers/ShowController.cs
@@ -28,16 +28,58 @@ namespace ShowWatch.Server.Controllers
         public IEnumerable<Show> Get()
         {
             // create shows and add them to a list
-            
-            
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new Show
+
+            List<Show> shows = new List<Show>()
             {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            })
-            .ToArray();
+                new Show() {    Title = "Fargo",
+                                ShowType = ShowType.BoxSet,
+                                Status = Status.Available,
+                                LatestSeason = 4, 
+                                NumEpisodes = 11, 
+                                Description = "American Black Comedy"},
+                new Show() {    Title = "Succession",
+                                ShowType = ShowType.BoxSet,
+                                Status = Status.NoReleaseDate,
+                                LatestSeason = 3,
+                                Description = "American Drama/Comedy based on the Roy family, " +
+                                "owners of a global media company"},
+                new Show() {    Title = "Upright",
+                                ShowType = ShowType.BoxSet,
+                                LatestSeason = 2,
+                                Description = "Fantastic comedy/drama following a musician " +
+                                "and teenager's adventures as they transport an upright piano " +
+                                "from Sydnet to Perth"},
+                new Show() {    Title = "Louis Theroux's Weird Weekends",
+                                ShowType = ShowType.Documentary,
+                                Status = Status.Available,
+                                LatestSeason = 4},
+                new Show() {    Title = "The Devil Next Door",
+                                ShowType = ShowType.Documentary,
+                                Status = Status.Available},
+                new Show() {    Title = "Vietnam",
+                                ShowType = ShowType.Documentary,
+                                Status = Status.Available},
+                new Show() {    Title = "Uncut Gem",
+                                ShowType = ShowType.Movie,
+                                Status = Status.NoReleaseDate },
+                new Show() {    Title = "Black Widow",
+                                ShowType = ShowType.Movie,
+                                Status = Status.NoReleaseDate,
+                                ReleaseDate = new DateTime(2021, 05, 7),
+                                IsKidFriendly = true,
+                                Description = "Latest in the Marvel series starring Scarlett Johansen"},
+                new Show() {    Title = "The SpongeBob Movie: Sponge on the Run",
+                                ShowType = ShowType.Movie,
+                                Status = Status.NoReleaseDate,
+                                ReleaseDate = new DateTime(2021, 05, 7),
+                                IsKidFriendly = true,
+                                Description = "Latest in the Marvel series starring Scarlett Johansen"},
+
+            };
+
+
+
+            return shows.ToArray();
         }
     }
 }
